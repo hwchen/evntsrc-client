836cd7c9a3cfd866903d71672bb25b8836126f3c
Walther Chen <walther.chen@gmail.com>
2019-11-13 07:47:11 -0500

    fix timer; use handle::default, poll delay from state machine

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,12 @@ use hyper::{client, http, Body, Client, Request, Response};
 use hyper_tls::HttpsConnector;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use tokio_timer::{timer, Timer};
 use tracing::{event, Level};
 
 // TODO add timeout
 pub struct Opts {
-    reconnection_time: u32, // in milliseconds
+    reconnection_delay: u64, // in milliseconds
     buffer_capacity: usize,
 }
 
@@ -16,7 +17,7 @@ impl EventSource {
     pub async fn new(url: &str) -> Result<EventSource, Box<dyn std::error::Error>> {
         // defaults
         let opts = Opts {
-            reconnection_time: 1000,
+            reconnection_delay: 1000,
             buffer_capacity: 1_000_000,
         };
 
@@ -44,11 +45,16 @@ impl EventSource {
 
         let body = resp.into_body();
 
+        // This uses the default timer associated with the tokio runtime.
+        // We don't have to manually set up a timer.
+        let timer_handle = timer::Handle::default();
+
         Ok(EventSource {
             client,
             url,
-            reconnection_time: opts.reconnection_time,
+            reconnection_delay: opts.reconnection_delay,
             last_event_id_string: "".to_owned(),
+            timer: timer_handle,
             buf: BytesMut::with_capacity(opts.buffer_capacity),
             body,
             state: State::Stream,
@@ -59,8 +65,9 @@ impl EventSource {
 pub struct EventSource {
     client: Client<HttpsConnector<client::HttpConnector>>,
     url: http::Uri,
-    reconnection_time: u32, // in milliseconds
+    reconnection_delay: u64, // in milliseconds
     last_event_id_string: String,
+    timer: timer::Handle,
     buf: BytesMut,
     body: Body,
     state: State,
@@ -70,9 +77,37 @@ impl Stream for EventSource {
     type Item = Result<Event, Box<dyn std::error::Error>>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-        let EventSource { ref mut body, ref mut buf, ref mut client, url, ref mut state, .. } = self.get_mut();
+        let EventSource {
+            ref mut client,
+            url,
+            reconnection_delay,
+            timer,
+            ref mut buf,
+            ref mut body,
+            ref mut state,
+            .. } = self.get_mut();
 
-        // early return for handling reconnect
+        // first handle reconnect and delay
+        if let State::Delay(ref mut delay) = state {
+            match delay.poll_unpin(cx) {
+                Poll::Pending => {
+                    event!(Level::DEBUG, "pending delay");
+                    return Poll::Pending;
+                },
+                Poll::Ready(_) => {
+                    event!(Level::DEBUG, "readyok, done delaying");
+                    let req = Request::builder()
+                        .method("GET")
+                        .uri((*url).clone())
+                        .header("Accept", "text/event-stream")
+                        .body(Body::empty())?;
+
+                    let resp_fut = client.request(req);
+
+                    *state = State::Reconnect(Box::pin(resp_fut));
+                },
+            }
+        }
         if let State::Reconnect(ref mut resp_fut) = state {
             match resp_fut.poll_unpin(cx) {
                 Poll::Pending => {
@@ -84,7 +119,7 @@ impl Stream for EventSource {
                     return Poll::Ready(Some(Err(format!("Could not reconnect: {}", err).into())));
                 },
                 Poll::Ready(Ok(resp)) => {
-                    event!(Level::DEBUG, "readyok, reconnecting");
+                    event!(Level::DEBUG, "readyok, reconnected");
 
                     // TODO 204 No Content means don't try to reconnect
                     *body = resp.into_body();
@@ -93,41 +128,58 @@ impl Stream for EventSource {
             }
         }
 
+        // Now handle stream
         match body.poll_next_unpin(cx) {
             Poll::Pending => { event!(Level::DEBUG, "pending"); Poll::Pending },
             Poll::Ready(None) => {
-                event!(Level::DEBUG, "stream ended, reconnect");
-                let req = Request::builder()
-                    .method("GET")
-                    .uri((*url).clone())
-                    .header("Accept", "text/event-stream")
-                    .body(Body::empty())?;
-
-                let mut resp_fut = client.request(req);
+                event!(Level::DEBUG, "stream ended, reconnect with delay");
+                let deadline = std::time::Instant::now() + std::time::Duration::from_millis(*reconnection_delay);
+                let mut delay_fut = timer.delay(deadline);
 
                 // We're still on the completed body (returned None), so have to kickstart
                 // polling a new task? Doesn't seem to work if we just pass return Poll::Pending
                 // and wait for the next round of polling.
                 //
                 // match block copied from above
-                match resp_fut.poll_unpin(cx) {
+                match delay_fut.poll_unpin(cx) {
                     Poll::Pending => {
-                        event!(Level::DEBUG, "pending reconnect");
-                        *state = State::Reconnect(Box::pin(resp_fut));
+                        event!(Level::DEBUG, "pending delay");
+                        *state = State::Delay(Box::pin(delay_fut));
                         return Poll::Pending;
                     },
-                    Poll::Ready(Err(err)) => {
-                        event!(Level::DEBUG, "readyerr reconnect err: {}", err);
-                        *state = State::Reconnect(Box::pin(resp_fut));
-                        return Poll::Ready(Some(Err(format!("Could not reconnect: {}", err).into())));
-                    },
-                    Poll::Ready(Ok(resp)) => {
-                        event!(Level::DEBUG, "readyok, reconnecting");
+                    Poll::Ready(_) => {
+                        event!(Level::DEBUG, "readyok, delaying");
 
-                        // TODO 204 No Content means don't try to reconnect
-                        *body = resp.into_body();
-                        *state = State::Stream;
-                        return Poll::Pending;
+                        // have to poll any other state that's not unit, there's no easy way to
+                        // change state otherise
+                        let req = Request::builder()
+                            .method("GET")
+                            .uri((*url).clone())
+                            .header("Accept", "text/event-stream")
+                            .body(Body::empty())?;
+
+                        let mut resp_fut = client.request(req);
+
+                        match resp_fut.poll_unpin(cx) {
+                            Poll::Pending => {
+                                event!(Level::DEBUG, "pending reconnect");
+                                *state = State::Reconnect(Box::pin(resp_fut));
+                                return Poll::Pending;
+                            },
+                            Poll::Ready(Err(err)) => {
+                                event!(Level::DEBUG, "readyerr reconnect err: {}", err);
+                                *state = State::Reconnect(Box::pin(resp_fut));
+                                return Poll::Ready(Some(Err(format!("Could not reconnect: {}", err).into())));
+                            },
+                            Poll::Ready(Ok(resp)) => {
+                                event!(Level::DEBUG, "readyok, reconnected");
+
+                                // TODO 204 No Content means don't try to reconnect
+                                *body = resp.into_body();
+                                *state = State::Stream;
+                                return Poll::Pending;
+                            },
+                        }
                     },
                 }
             },
@@ -137,17 +189,12 @@ impl Stream for EventSource {
                     Err(err) => {
                         // TODO check all error cases
                         if err.is_closed() || err.is_canceled() {
-                            event!(Level::DEBUG, "error, reconnect");
+                            event!(Level::DEBUG, "error, reconnect with delay");
 
-                            let req = Request::builder()
-                                .method("GET")
-                                .uri((*url).clone())
-                                .header("Accept", "text/event-stream")
-                                .body(Body::empty())?;
+                            let deadline = std::time::Instant::now() + std::time::Duration::from_millis(*reconnection_delay);
+                            let delay_fut = timer.delay(deadline);
 
-                            let resp_fut = client.request(req);
-
-                            *state = State::Reconnect(Box::pin(resp_fut));
+                            *state = State::Delay(Box::pin(delay_fut));
 
                             return Poll::Pending;
                         } else {
@@ -178,6 +225,7 @@ impl Stream for EventSource {
 enum State {
     Stream,
     Reconnect(Pin<Box<dyn Future<Output=Result<Response<Body>, hyper::error::Error>>>>),
+    Delay(Pin<Box<dyn Future<Output=()>>>),
 }
 
 pub enum Event {
